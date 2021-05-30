0fd65bfbb8fc66061bff911dbf782d6706891f60
Rakshya Pokhrel <rakshya.pokhrel1@gmail.com>
2021-05-30 21:24:21 +0545

    First Project Setup

---
+++ src/main/java/com/RentSewa/Rent/Sewa/RentSewaApplication.java
@@ -0,0 +1,13 @@
+ package com.RentSewa.Rent.Sewa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentSewaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentSewaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RentSewa/Rent/Sewa/RentSewaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RentSewa.Rent.Sewa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentSewaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
